22e025068709969dba37b72e4a8c390bbf28e436
karturval1526 <vealquezcar@gmail.com>
2026-09-20 13:56:54 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="roadpilot-v4-cache";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match("./index.html")))));
